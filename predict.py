0a7ec45e0e6adcd2930ac550ca8d35871a7a8843
Srushti-Gadivaddar <gadivaddarsrushti@gmail.com>
2026-04-30 22:36:41 +0530

    New Chages

--- predict.py
+++ predict.py
@@ -3,11 +3,11 @@ from pydantic import BaseModel
 import joblib
 from scipy.sparse import hstack
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 
 app = FastAPI()
 
-
+port = int(os.environ.get("PORT", 10000))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
